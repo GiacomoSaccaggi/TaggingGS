309b441dfecd53b9e5e00884095138292af88e63
Giacomo Saccaggi <44114264+GiacomoSaccaggi@users.noreply.github.com>
2022-02-16 16:57:01 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 # pdoc -o ./html GSTag
 setup(name='tagginggs',
-      version='0.1.0',
+      version='0.1.1',
       description='Easily Tag',
       url='',
       author='Saccaggi Giacomo',
@@ -11,7 +11,8 @@ setup(name='tagginggs',
       packages=find_packages(),
       include_package_data=True,
       install_requires=[
-            'Flask', 'Pillow', 'tensorflow', 'scikit-learn', 'matplotlib', 'tqdm', 'numpy'
+            'Flask~=2.0.2', 'Pillow~=8.3.2', 'tensorflow~=2.6.0', 'scikit-learn~=1.0', 'matplotlib~=3.4.3', 'tqdm~=4.62.3', 'numpy~=1.19.5', 'flask_httpauth~=4.4.0','gunicorn~=20.1.0','spacy~=3.1.3'
       ],
       zip_safe=False
       )
+
